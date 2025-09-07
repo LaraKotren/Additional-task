6da5dc1a3b5d9978005cdddb3a123dd3285aeaa5
LaraKotren <lauramihajlova315@gmail.com>
2025-09-08 00:34:15 +0300

    Update AT.py

--- AT.py
+++ AT.py
@@ -8,10 +8,11 @@ y = np.log(x)
 plt.figure(figsize=(10, 5))
 plt.plot(x, y, label='ln(x)', color='blue')
 
-plt.title('График натурального логарифма')
+plt.title('Р“СЂР°С„РёРє РЅР°С‚СѓСЂР°Р»СЊРЅРѕРіРѕ Р»РѕРіР°СЂРёС„РјР°')
 plt.xlabel('x')
 plt.ylabel('ln(x)')
 plt.grid(True)
 plt.legend()
 
 plt.show()
+
